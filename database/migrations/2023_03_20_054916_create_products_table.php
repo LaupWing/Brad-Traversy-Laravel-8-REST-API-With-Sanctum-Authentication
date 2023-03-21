cfc8c3b0710a14601f9ec012ec6284ad73e31776
LaupWing <loc_strict@hotmail.com>
2023-03-21 05:26:35 +0100

    migratiosn

--- database/migrations/2023_03_20_054916_create_products_table.php
+++ database/migrations/2023_03_20_054916_create_products_table.php
@@ -6,22 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   /**
+    * Run the migrations.
+    */
+   public function up(): void
+   {
+      Schema::create('products', function (Blueprint $table) {
+         $table->id();
+         $table->string("name");
+         $table->string("slug");
+         $table->string("description")->nullable();
+         $table->decimal("price", 5, 2);
+         $table->timestamps();
+      });
+   }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('products');
-    }
+   /**
+    * Reverse the migrations.
+    */
+   public function down(): void
+   {
+      Schema::dropIfExists('products');
+   }
 };
