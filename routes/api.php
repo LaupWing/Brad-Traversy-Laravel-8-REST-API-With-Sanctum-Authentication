1c0a8f73f709bd777ce81449b883eed23c07b1ca
LaupWing <loc_strict@hotmail.com>
2023-03-21 05:42:50 +0100

    resourd

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource("products", ProductController::class);
 
-Route::get("/products", [ProductController::class, "index"]);
+// Route::get("/products", [ProductController::class, "index"]);
 
-Route::post("/products", [ProductController::class, "store"]);
+// Route::post("/products", [ProductController::class, "store"]);
+
+// Route::post("/products/", [ProductController::class, "store"]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
    return $request->user();
