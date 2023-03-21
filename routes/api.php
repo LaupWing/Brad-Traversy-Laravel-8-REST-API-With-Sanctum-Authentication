6daa0b54cd28124271fc05901b0825082461031a
LaupWing <loc_strict@hotmail.com>
2023-03-21 05:30:05 +0100

    Produc tal

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get("products", function () {
+Route::get("/products", function () {
+   return Product::all();
+});
+
+Route::post("/products", function () {
    return "products";
 });
 
